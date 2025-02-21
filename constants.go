6df5f182e5aedfa7090a416ebfc6e2e15dade06d
mike tihonchik <angry.guy.soft@gmail.com>
2025-02-21 20:04:17 +0200

    get sample NLM API running

---
+++ constants.go
@@ -0,0 +1,3 @@
+package main
+
+const BASE_URL = "https://clinicaltables.nlm.nih.gov/api/icd10cm/v3"
--- main.go
+++ main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-    fmt.Println("Hello, World!")
+	router := gin.Default()
+	router.GET("/icd10codes", getICD10Code)
+
+	router.Run("localhost:8080")
+}
+
+func getICD10Code(c *gin.Context) {
+	resp, err := http.Get(BASE_URL + "/search?sf=code,name&terms=tuberc")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+	}
+	var response []interface{}
+	json.Unmarshal(body, &response)
+	fmt.Println(response[0])
 }
